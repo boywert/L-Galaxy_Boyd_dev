70dffd19ac6da179aa51fe15def7901a89de1305
Chaichalit Srisawat <boywert@gmail.com>
2015-08-31 18:18:18 +0100

    quick commit

--- code/recipe_misc.c
+++ code/recipe_misc.c
@@ -442,12 +442,12 @@ void add_to_luminosities(int p, double mstars, double time, double metallicity)
     			                  fmet2 * (f1 * LumTables[j][metindex + 1][0][tabindex] +
 					                       f2 * LumTables[j][metindex + 1][0][tabindex + 1]));
     	  Gal[p].Lum[j][outputbin] += LuminosityToAdd;
-	  if((j ==5) && (age >= 0.)) {
-	    printf("age = %0.10f metal = %0.10f SFR = %0.10f\n",age,metallicity,Gal[p].Sfr * UnitMass_in_g / UnitTime_in_s * SEC_PER_YEAR / SOLAR_MASS);
-	    printf("tabindex = %d metindex = %d\n",tabindex,metindex);
-	    printf("X1 = %0.10f factor = %0.10f\n",X1,factor);
-	    printf("+Lum: %0.10f = %0.10f\n", LuminosityToAdd,Gal[p].Lum[j][outputbin]);
-	  }
+	  /* if((j ==5) && (age >= 0.)) { */
+	  /*   printf("age = %0.10f metal = %0.10f SFR = %0.10f\n",age,metallicity,Gal[p].Sfr * UnitMass_in_g / UnitTime_in_s * SEC_PER_YEAR / SOLAR_MASS); */
+	  /*   printf("tabindex = %d metindex = %d\n",tabindex,metindex); */
+	  /*   printf("X1 = %0.10f factor = %0.10f\n",X1,factor); */
+	  /*   printf("+Lum: %0.10f = %0.10f\n", LuminosityToAdd,Gal[p].Lum[j][outputbin]); */
+	  /* } */
     	  /*luminosity used for extinction due to young birth clouds */
     	  if(age <= tbc)
 	        Gal[p].YLum[j][outputbin] += LuminosityToAdd;
