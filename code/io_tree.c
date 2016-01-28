56412cf01abf03d4297807421b8f69ace1143da6
Chaichalit Srisawat <boywert@gmail.com>
2016-01-28 19:31:10 +0000

    quick commit

--- code/io_tree.c
+++ code/io_tree.c
@@ -247,7 +247,7 @@ void load_tree_hdf5(int filenr, int *totNHalos) {
   nt++;
   strcpy(tag[nt], "HaloIDs_Data_FirstHaloInFOFgroup");
   addr[nt] = HaloIDs_Data_FirstHaloInFOFgroup;
-  data_type[nt] = longtype;
+  data_type[nt] = doubletype;
   nt++;
   strcpy(tag[nt], "HaloIDs_Data_NextHaloInFOFgroup");
   addr[nt] = HaloIDs_Data_NextHaloInFOFgroup;
@@ -353,7 +353,7 @@ void load_tree_hdf5(int filenr, int *totNHalos) {
   errorFlag = 0;
   for(i = 5; i < nt; i++) {
     if(!found_hdf5[i]) {
-      printf("Error. I miss a value for tag '%s' in HDF5 input file '%s'.\nAll values will be set to 0.\n", addr[i], buf);
+      printf("Error. I miss a value for tag '%s' in HDF5 input file '%s'.\nAll values will be set to generic.\n", addr[i], buf);
     }
   }    
 
