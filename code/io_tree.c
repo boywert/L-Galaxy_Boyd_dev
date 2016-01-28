f6813b40437c7e5ac3f5c3f5a7be214b45363f6d
Chaichalit Srisawat <boywert@gmail.com>
2016-01-28 18:06:57 +0000

    quick commit

--- code/io_tree.c
+++ code/io_tree.c
@@ -285,8 +285,8 @@ void load_tree_hdf5(int filenr, int *totNHalos) {
       memb_name = H5Tget_member_name( native_type, i);
       printf("Member: %s\n",memb_name);
       for(j=0;j<nt;j++) {
-	//if(strcmp((addr[j], memb_name) == 0)
-	printf("%s => %s ::: %s\n",tag[j], addr[j],memb_name);
+	if(strcmp((addr[j], memb_name) == 0)
+	   printf("%s => %s ::: %s\n",tag[j], addr[j],memb_name);
       }
       if (H5Tequal (memb_id, H5T_STD_I32LE))
 	printf ("  Member %i:  Type is H5T_STD_I32LE\n", i);
